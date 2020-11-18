35a7cb0390d1ae4355af1ca7a152cf194a2230e0
Ophelie_Marot <ophelie.marot@gmail.com>
2020-11-18 13:48:30 +0100

    Profile

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -26,11 +26,37 @@ class ArticlesController extends AbstractController
 
             //Je stocke dans une variable l'appel de l'article par son id dans l'url
          $article = $articles[$id];
-         
+
             //J'utilise la fontion render, enfant de abstractController pour lier mon fichier html.twig
          return $this->render('article.html.twig', [
              'article' => $article
          ]);
 
         }
+
+    /**
+     * @Route("/profile", name="page_profile")
+     */
+
+    // Je crée une nouvelle fonction contenant un tableau avec les infos de profile
+     public function profileShow ()
+        {
+            $profile = [
+                "firstname" => "Flantier",
+                "name" => "Noel",
+                "age" => 40,
+                "job" => "secret agent",
+                "active" => true
+            ];
+
+            //Je retourne grâce a mon render mon fichier twig vers mon navigateur
+            return $this->render('article.html.twig',[
+
+                //Je stock mes infos dans une variable que je pourraîs utiliser das mon fichier twig
+                'profile' => $profile
+            ]);
+
+
+        }
+
     }
